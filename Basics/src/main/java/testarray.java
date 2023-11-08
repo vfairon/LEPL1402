47ce390bcfcd0450304e502dadc2462eff76634d
vfairon <vfairon@gmail.com>
2023-11-08 16:18:33 +0100

    lol...

--- Basics/src/main/java/testarray.java
+++
@@ -1,9 +0,0 @@
-public class testarray {
-    public static void main(String[] args) {
-        int[] array = new int[3];
-        for(int element:array){
-            System.out.println(element);
-        }
-
-    }
-}
